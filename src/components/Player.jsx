75c92c13ada7905092d1d4302bb23a7609f618bc
MuhammadArsalanShah <arsalanshah408@yahoo.com>
2024-08-26 12:51:38 +0500

    Making the player name and edit button interactive

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
+
 export default function Player({name, symbol}) {
+
+  const [ isEditing, setIsEditing ] = useState(false)
+
+  function handleEditClick() {
+    if (!isEditing) {
+      setIsEditing(true)
+    } else {
+      setIsEditing(false);
+    }
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+
+  if (isEditing) {
+    playerName = <input type="text" name="" id="" required />
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{!isEditing ? 'Edit' : 'Save'}</button>
     </li>
   );
 }
